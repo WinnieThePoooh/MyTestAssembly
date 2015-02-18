c4b692a4536c3361b0ad29056b6df26852d6ba5a
Anton.Spilnyy <aspilnyy@gmail.com>
2015-02-18 12:31:41 +0300

    add project

---
+++ MyTestAssembly/MyTestPublicClassInRootNamespace.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace JetBrains.WinnieThePooh.MyTestAssembly
+{
+  public class MyTestPublicClassInRootNamespace
+  {
+    public class MyInnerClass { }
+  }
+
+  public class MyTestProtectedClassInRootNamespace
+  {
+  }
+
+  public class MyTestPrivateClassInRootNamespace
+  {
+  }
+
+  public interface IMyTestInterfaceInRootNamespace
+  {
+  }
+
+  public struct MyPublicStruct
+  {
+  }
+
+  public enum MyPublicEnum
+  {
+    ToBe = 0,
+    NotToBe = 1
+  }
+}
+
+namespace JetBrains.WinnieThePooh.MyTestAssembly.MinorNamespace
+{
+  public class MyTestPublicClassInMinorNamespace
+  {
+  }
+
+  public class MyTestProtectedClassInMinorNamespace
+  {
+  }
+
+  public class MyTestPrivateClassInMinorNamespace
+  {
+  }
+
+  public interface IMyTestInterfaceInMinorNamespace
+  {
+  }
+}
